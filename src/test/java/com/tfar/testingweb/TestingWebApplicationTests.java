6e704921707ea89756164ab981db7902aa537fca
projet@five-consulting.com <projet@five-consulting.com>
2022-05-28 15:43:29 +0100

    first commit

---
+++ src/test/java/com/tfar/testingweb/TestingWebApplicationTests.java
@@ -0,0 +1,43 @@
+package com.tfar.testingweb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.tfar.testingweb.controller.HomeController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class TestingWebApplicationTests {
+/*
+	@Autowired
+	private HomeController controller;
+	*/
+	@Autowired
+	private MockMvc mockMvc;
+	
+/*	@Test
+	void contextLoads()  throws Exception  {
+		assertThat(controller).isNotNull();
+	}
+	*/
+	
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
+	}
+}
+	
+
+
