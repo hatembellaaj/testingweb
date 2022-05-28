889fbcf95fb9f465cb7e0cc35c5ead3bca975f2a
projet@five-consulting.com <projet@five-consulting.com>
2022-05-28 16:23:41 +0100

    adding employee model and test

--- src/test/java/com/tfar/testingweb/EmployeeControllerTest.java
+++ src/test/java/com/tfar/testingweb/EmployeeControllerTest.java
@@ -1,49 +1,35 @@
 package com.tfar.testingweb;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.tfar.testingweb.controller.HomeController;
-
 @SpringBootTest
 @AutoConfigureMockMvc
+public class EmployeeControllerTest {
 
-class TestingWebApplicationTests {
-
-/*	@Autowired
-	private HomeController controller;*/
-	
 	@Autowired
 	private MockMvc mockMvc;
-	
-/*	@Test
-	void contextLoads()  throws Exception  {
-		assertThat(controller).isNotNull();
-	}*/
-	
-	
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
-	}
-	
 
-	
-	
-	
+	@Test
+	public void testEmployee() throws Exception {
+		mockMvc.perform(get("/employee")).andExpect(status().isOk())
+		.andExpect(content().contentType("application/json"))
+		.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+		.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+	}
+
+
+
 }
-	
+
 
 
